332932d24adc246ea522e8d25827269b57dbcf47
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2024-03-12 16:21:19 +0000

    last staging

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "6AMWFVESiY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+# always check this
 
 ALLOWED_HOSTS = [
     "8000-vladaalek-vladimirdjang-ge32fa0jxmp.ws-eu108.gitpod.io",
