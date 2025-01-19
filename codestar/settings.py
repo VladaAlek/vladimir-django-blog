98476e46a7593a6de59f48b661f101f67bfbb872
Vladimir Aleksic <vladimiraleksic.nis@gmail.com>
2025-01-19 15:36:29 +0000

    deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,8 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "6AMWFVESiY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -61,7 +59,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'codestar.urls'
-
+#
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
